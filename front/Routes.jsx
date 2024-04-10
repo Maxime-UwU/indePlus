853fe1b3d595de60b9f600dbdc974db17acd2607
Maxime Maxime <maximenicolas1512@gmail.com>
2024-04-10 11:54:55 +0200

    add login route

--- front/Routes.jsx
+++ front/Routes.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ const Routes = () => {
     return (
         <Stack.Navigator initialRouteName='Home'>
             <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Login" component={Login} />
         </Stack.Navigator>
     );
 };
