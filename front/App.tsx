a3cfb94d559a45c38b74e52334f784bb9a5bd268
KillianVendewinkele <killian.vendewinkele@gmail.com>
2024-05-17 08:36:55 +0200

    notif ask permissions and appears correctly

--- front/App.tsx
+++ front/App.tsx
@@ -3,26 +3,15 @@ import { SafeAreaView, Platform, PermissionsAndroid, PermissionStatus } from 're
 import Routes from './Routes'; 
 import { NavigationContainer } from '@react-navigation/native';
 
-const PERMISSION_REQUEST_CODE = 123;
-
 const App: React.FC = () => {
 
   useEffect(() => {
     const requestNotificationPermission = async () => {
       try {
         if (Platform.OS === 'android') {
-          const permissions: {[key: string]: PermissionStatus} = {
-            'android.permission.INTERNET': PermissionsAndroid.RESULTS.GRANTED,
-            'android.permission.RECEIVE_BOOT_COMPLETED': PermissionsAndroid.RESULTS.GRANTED,
-            'android.permission.VIBRATE': PermissionsAndroid.RESULTS.GRANTED
-          };
-          const granted = await PermissionsAndroid.requestMultiple(Object.keys(permissions));
-
-          for (const permission in granted) {
-            if (granted[permission] !== PermissionsAndroid.RESULTS.GRANTED) {
-              console.log(`Permission ${permission} refusée`);
-            }
-          }
+          await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+          );
         }
       } catch (err) {
         console.warn(err);
