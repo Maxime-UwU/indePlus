1fa307863fae6fe0854a219b97dbd566cdf256c0
KillianVendewinkele <killian.vendewinkele@gmail.com>
2024-06-05 21:29:34 +0200

    add some components

--- front/Routes.jsx
+++ front/Routes.jsx
@@ -5,17 +5,19 @@ import Login from './screens/Login';
 // import Register from './screens/Register';
 import SplashScreen from './screens/SplashScreen';
 import SplashScreen2 from './screens/SplashScreen2';
+import ListGames from './screens/ListGames';
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
     return (
-        <Stack.Navigator initialRouteName='Login'>
+        <Stack.Navigator initialRouteName='ListGames'>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Login" component={Login} />
             {/* <Stack.Screen name="Register" component={Register} /> */}
             <Stack.Screen name="SplashScreen" component={SplashScreen} />
             <Stack.Screen name="SplashScreen2" component={SplashScreen2} />
+            <Stack.Screen name="ListGames" component={ListGames} />
         </Stack.Navigator>
     );
 };
--- front/screens/ListGames.jsx
+++ front/screens/ListGames.jsx
@@ -8,15 +8,31 @@ import {
   View,
   Image,
   FlatList,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
+import GameCarrousel from '../components/templates/GameCarrousel';
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.backgroundStyle}>
       <ScrollView nestedScrollEnabled>
-        <View  style={styles.fullPage}>
-          
+        <View style={styles.fullPage}>
+          <View style={styles.searchArea}>
+            <View style={styles.searchBarArea}>
+              <TextInput style={styles.searchBar}></TextInput>
+              <TouchableOpacity style={styles.searchButton}></TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.filterOpenButton}></TouchableOpacity>
+          </View>
+          <View style={styles.filterArea}>
+            
+          </View>
+          <GameCarrousel games={[
+            { id: 1, title: "Spell Swap", studio: "Teagher Studio", image: require('./../components/images/spellswapthumbnail.jpg') },
+            { id: 2, title: "Nom du jeu 2", studio: "Studio 2", image: require('./../components/images/spellswapthumbnail.jpg') },
+            { id: 3, title: "Nom du jeu 3", studio: "Studio 3", image: require('./../components/images/spellswapthumbnail.jpg') }
+          ]} />
         </View>
       </ScrollView>
     </SafeAreaView>
