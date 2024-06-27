fa2bd219f847182449657c6e7bbc98d992e4f6f8
Tristan NEUMEIER <90679506+Archantrax@users.noreply.github.com>
2024-06-27 23:22:31 +0200

    lasttentative

---
+++ back/.fly/FlySymfonyRuntime.php
@@ -0,0 +1,13 @@
+namespace App;
+
+use Symfony\Component\Runtime\SymfonyRuntime;
+
+class FlySymfonyRuntime extends SymfonyRuntime
+{
+    public function __construct(array $options = [])
+    {
+        $options['disable_dotenv'] = true;
+
+        parent::__construct($options);
+    }
+}
